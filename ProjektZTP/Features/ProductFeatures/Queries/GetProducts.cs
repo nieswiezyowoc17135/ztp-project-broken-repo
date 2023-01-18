cbc66004758e69cf92c5c2d99fc4f73c55c2f8cd
Ryszard Rutkowski <80615@student.pb.edu.pl>
2023-01-18 16:42:00 +0100

    Generowanie diagramów do folderu ProjektZTP

--- ProjektZTP/Features/ProductFeatures/Queries/GetProducts.cs
+++ ProjektZTP/Features/ProductFeatures/Queries/GetProducts.cs
@@ -1,6 +1,7 @@
-﻿using System.Runtime.CompilerServices;
-using ProjektZTP.Repository.Interfaces;
+﻿using ProjektZTP.Repository.Interfaces;
 using static ProjektZTP.Mediator.Abstract;
+using System.Net;
+using static System.Net.WebRequestMethods;
 
 namespace ProjektZTP.Features.ProductFeatures.Queries;
 
@@ -12,7 +13,7 @@ public class GetProducts
     {
         private readonly IProductsRepository _repository;
 
-        public Handler (IProductsRepository repository)
+        public Handler(IProductsRepository repository)
         {
             _repository = repository;
         }
@@ -21,6 +22,11 @@ public class GetProducts
         {
             var tmpList = new List<ProductDTO>();
             var result = await _repository.GetProducts();
+
+            string names = "";
+            string value = "";
+            string taxes = "";
+            string amounts = "";
             foreach (var product in result)
             {
                 var tmp = new ProductDTO(
@@ -29,8 +35,32 @@ public class GetProducts
                     product.Amount,
                     product.Vat);
                 tmpList.Add(tmp);
+
+                if(value == null)
+                {
+                    names = product.Name;
+                    value = (product.Price * product.Amount).ToString();
+                    taxes=product.Vat.ToString();
+                    amounts = product.Amount.ToString();
+                }
+                else
+                {
+                    names = product.Name + "," + names;
+                    value = (product.Price * product.Amount).ToString() + "," + value;
+                    taxes = product.Vat.ToString() + "," + taxes;
+                    amounts = product.Amount.ToString() + "," + amounts;
+                }
             }
 
+            string link = "https://quickchart.io/chart/render/sm-5e8581ce-0ed7-43ca-9658-13889fc65104?title= Magazine value&labels=" + names + "&data1=" + value;
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(link, "WartośćMagazynu.png");
+
+            link= "https://quickchart.io/chart/render/sm-89cd9e8a-a2a9-4cf5-bdc0-3706831298b0?title= Products Taxes&labels=" + names + "&data1=" + taxes;
+            webClient.DownloadFile(link, "WysokoscVat.png");
+
+            link = "https://quickchart.io/chart/render/sm-66cffd1b-318e-42d3-9b53-5998c3660026?title= Price/Tax &labels=" + names + "&data1=" + value + "&data2=" + amounts;
+            webClient.DownloadFile(link, "Cena&Ilosc.png");
             return new GetProductsResult(tmpList);
         }
     }
